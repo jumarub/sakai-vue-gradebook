c0e35bc87345fc6cc12eb991fc93df4b7ff79ca9
Juan Manuel Rubiales de la Cruz <91719773+jumarub@users.noreply.github.com>
2024-10-11 13:29:49 +0200

    flowbite vue fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+module.exports = {
   content: [
-    "./node_modules/flowbite/**/*.js",
+    'node_modules/flowbite-vue/**/*.{js,jsx,ts,tsx,vue}',
+    'node_modules/flowbite/**/*.{js,jsx,ts,tsx}'
   ],
-  theme: {
-    extend: {},
-  },
   plugins: [
-    require('flowbite/plugin')
+      require('flowbite/plugin')
   ],
+  theme: {}
 }
 
